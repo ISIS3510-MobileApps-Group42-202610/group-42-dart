9ed889a94714e9ab60ebf4692c803acc0197024c
Adrian Velasquez <141940483+adrianvelasquezs@users.noreply.github.com>
2026-03-16 11:54:37 -0500

    definicion de los estados para el bloc

---
+++ lib/auth/bloc/auth_state.dart
@@ -0,0 +1,70 @@
+// estados utilizados en el bloc
+
+import 'package:equatable/equatable.dart';
+import '../models/auth_models.dart';
+
+abstract class AuthState extends Equatable {
+  const AuthState();
+
+  @override
+  List<Object> get props => [];
+}
+
+
+// El app inicia por primera vez
+class AuthInitial extends AuthState {
+  const AuthInitial();
+}
+
+// operacion asincrona
+class AuthLoading extends AuthState {
+  const AuthLoading();
+}
+
+// el usuario se autentico (login, registro, sesion restaurada con token)
+class AuthAuthenticated extends AuthState {
+  final AuthUser user;
+  final String accessToken;
+
+  const AuthAuthenticated({required this.user, required this.accessToken});
+
+  @override
+  List<Object> get props => [user, accessToken];
+}
+
+
+// no hay token
+class AuthUnauthenticated extends AuthState {
+  const AuthUnauthenticated();
+}
+
+
+// error ocurrio durante el login o registro del usuario
+class AuthError extends AuthState {
+  final String message;
+
+  const AuthError({required this.message});
+
+  @override
+  List<Object> get props => [message];
+}
+
+// enum para las posibles acciones que se pueden realizar, sin contar login y registro
+enum AuthAction {
+  forgotPassword,
+  resetPassword,
+  deleteAccount,
+}
+
+// operacion exitosa (no login o registro)
+class AuthActionSuccess extends AuthState {
+  final String message;
+  final AuthAction action;
+
+  const AuthActionSuccess({required this.message, required this.action});
+
+  @override
+  List<Object> get props => [message, action];
+}
+
+
