083efd0429c9389da1ea44dc9a23eea6f11f871f
Adrian Velasquez <141940483+adrianvelasquezs@users.noreply.github.com>
2026-03-18 18:29:25 -0500

    el widget de analitica

---
+++ lib/analytics/analytics_providers.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'bloc/analytics_bloc.dart';
+import 'data/analytics_api_client.dart';
+import 'data/analytics_dio_client.dart';
+import 'data/device_info_service.dart';
+
+class AnalyticsProviders extends StatelessWidget {
+  final Widget child;
+  final String analyticsBaseUrl;
+
+  const AnalyticsProviders({
+    super.key,
+    required this.child,
+    required this.analyticsBaseUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final dio = createAnalyticsDio(baseUrl: analyticsBaseUrl);
+    final apiClient = AnalyticsApiClient(dio: dio);
+    final deviceInfo = DeviceInfoService();
+
+    return BlocProvider(
+      create: (_) => AnalyticsBloc(
+        apiClient: apiClient,
+        deviceInfo: deviceInfo,
+      ),
+      child: child,
+    );
+  }
+}
